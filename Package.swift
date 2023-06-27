abc398f9f411788a21ac3a826bfa93b624f7eb88
Stef Kors <stef.kors@gmail.com>
2023-06-27 10:42:15 +0200

    Add Symbol names

--- Package.swift
+++ Package.swift
@@ -5,17 +5,20 @@ import PackageDescription
 
 let package = Package(
     name: "SFSymbolKit",
+    platforms: [.macOS(.v11), .iOS(.v13), .tvOS(.v13), .watchOS(.v6), .macCatalyst(.v13)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "SFSymbolKit",
             targets: ["SFSymbolKit"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/lukepistrol/SFSymbolsMacro.git", from: "0.1.0")
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "SFSymbolKit"),
+        .target(name: "SFSymbolKit", dependencies: ["SFSymbolsMacro"]),
         .testTarget(
             name: "SFSymbolKitTests",
             dependencies: ["SFSymbolKit"]),
